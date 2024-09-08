36b4c2fe4117ed783f0b41c3971740fa3daff79f
maishiro <16446436+maishiro@users.noreply.github.com>
2024-09-08 15:22:44 +0900

    ページ表示の初期設定

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using LogViewer.Hubs;
+using LogViewer.Models;
 using LogViewer.Services;
 using Serilog;
 
@@ -22,6 +23,22 @@ builder.Host.UseSerilog();
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
 builder.Services.AddHostedService<LogMonitorService>(); // LogMonitorServiceを登録
+
+
+// XML設定ファイルの読み込み
+var xmlConfigService = new XmlConfigService("config/config.xml");
+var xmlConfig = xmlConfigService.LoadConfig();
+//
+var pageConfig = new PageConfig
+{
+    PageTitle = xmlConfig.Element("pageTitle")?.Value,
+    PageDescription = xmlConfig.Element("pageDescription")?.Value,
+    RefreshInterval = int.Parse(xmlConfig.Element("refreshInterval")?.Value ?? "5000"),
+    MaxEntries = int.Parse(xmlConfig.Element("maxEntries")?.Value ?? "100")
+};
+// PageConfigをDIコンテナに登録
+builder.Services.AddSingleton( pageConfig );
+
 
 var app = builder.Build();
 
---
+++ Services/XmlConfigService.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+
+namespace LogViewer.Services
+{
+    public class XmlConfigService
+    {
+        private readonly string _configPath;
+
+        public XmlConfigService( string configPath )
+        {
+            _configPath = configPath;
+        }
+
+        public XElement LoadConfig()
+        {
+            return XElement.Load( _configPath );
+        }
+    }
+}
